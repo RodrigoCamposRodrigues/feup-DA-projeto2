7218819b14cbf92028807aed799d8797a4e558f7
lulascoca <kekwm8@gmail.com>
2023-05-30 15:34:17 +0100

    menu created

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,16 @@
 #include "utils/csv_reader.h"
 #include "utils/graph.h"
 #include "manager.h"
+#include "menu/menu.h"
 
 int main() {
-    Manager manager("../dataset/Extra_Fully_Connected_Graphs/edges_900.csv");
+//    Manager manager("../dataset/Extra_Fully_Connected_Graphs/edges_900.csv");
 
-    manager.initialize_graphs_with_1_file();
+//    manager.initialize_graphs_with_1_file();
     //manager.printGraph();
     //manager.backtrack_tsp();
-    manager.triangularApproximation();
+//    manager.triangularApproximation();
+    Menu menu = Menu();
+    menu.menuLoop();
     return 0;
 }
